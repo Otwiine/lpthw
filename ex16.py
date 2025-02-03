4c7e148b5f72161ca7bfe26845cc0fbc37cd5b34
Otwiine Olweny <otwiine@gmail.com>
2025-02-03 17:04:03 +0300

    Adding comments

--- ex16.py
+++ ex16.py
@@ -1,27 +1,33 @@
 from sys import argv
 
+# Unpack argv 
 script, filename = argv
 
 print(f"Were going to erase {filename}.")
 print("If you don't want that hit CTRL-C (^C)")
 print("If you do want that, hit RETURN.")
 
+# Ask user for input
 input("?")
 
+# Open the file in write mode
 print("Opening the file...")
 target = open(filename, 'w')
 
-print("Truncating the file. Goodbye!")
-target.truncate()
+# Not needed because 'w' mode truncates the file
+# print("Truncating the file. Goodbye!")
+# target.truncate()
 
 print("Now i'm going to ask you for three lines.")
 
+# Prompt the user for input to assign to the variables
 line1 = input("line 1: ")
 line2 = input("line 2: ")
 line3 = input("line 3: ")
 
 print("I'm going to write these to the file.")
 
+# Write the new lines to the file, using newline characters to skip lines
 target.write(line1)
 target.write("\n")
 target.write(line2)
@@ -29,5 +35,6 @@ target.write("\n")
 target.write(line3)
 target.write("\n")
 
+# Close the file using .close()
 print("And finally, we close it.")
 target.close()
