4b674172142ea35dc082d0623f0ddbb77c08e736
Otwiine Olweny <otwiine@gmail.com>
2025-02-03 17:16:26 +0300

    Simplification of code

--- ex16.py
+++ ex16.py
@@ -28,12 +28,7 @@ line3 = input("line 3: ")
 print("I'm going to write these to the file.")
 
 # Write the new lines to the file, using newline characters to skip lines
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+target.write(line1 + "\n" + line2 + "\n" + line3 + "\n")
 
 # Close the file using .close()
 print("And finally, we close it.")
