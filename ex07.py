6565b3d9d8bd2f725fb3c0320172163f53ef8b8c
Otwiine Olweny <otwiine@gmail.com>
2025-02-04 16:22:59 +0300

    Punctuation fixes on comments

--- ex07.py
+++ ex07.py
@@ -1,13 +1,16 @@
-# print out string
+# Print out string
 print("Mary had a little lamb.")
-# print out string and replace {} with parameter in format
+
+# Print out string and replace {} with parameter in format
 print("It's fleece was white as {}.".format('snow'))
-# print out string
+
+# Print out string
 print("And everywhere that Mary went.")
-# multiply a single string character by 10 to print it out 10 times
+
+# Multiply a single string character by 10 to print it out 10 times
 print("." * 10) # what'd that do?
 
-# define single character variables
+# Define single character variables
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -21,7 +24,7 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
-#Concatenate the different variables and print them out
-# watch end = ' ' at the end. try removing it and see what happens
-print(end1 + end2 + end3 + end4 + end5 + end6,  end=' ')
+# Concatenate the different variables and print them out
+# watch end = ' ' at the end. try removing it and see what happens (It puts Burger on the next line) 
+print(end1 + end2 + end3 + end4 + end5 + end6,  end=' ') # (Space charcter)
 print(end7 + end8 + end9 + end10 + end11 + end12)
