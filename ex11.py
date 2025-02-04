1f095c78eba7071e88445ea703726bd39d9a8785
Otwiine Olweny <otwiine@gmail.com>
2025-02-04 16:25:37 +0300

    Addition of Study Drill

--- ex11.py
+++ ex11.py
@@ -6,3 +6,13 @@ print("How much do you weigh?", end= ' ')
 weight = input()
 
 print(f"So, you're {age} years old, {height} tall and {weight} heavy.")
+
+# Study Drill
+print("What is your name?", end=' ')
+name = input()
+print(f"Hi {name}!")
+print("Where do you live?", end=' ')
+location = input()
+print(f"{location}! Thats lovely")
+
+# end=' ' helps keep the prompt and user input on the same line
