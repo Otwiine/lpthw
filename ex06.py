1ec7abf3bdd48e5b3c75aa7e2a841a88c43564c5
Otwiine Olweny <otwiine@gmail.com>
2025-02-04 16:21:52 +0300

    Punctuation fixes on comments

--- ex06.py
+++ ex06.py
@@ -1,23 +1,23 @@
 # Define a variable and assign it a variable of 10
 types_of_people = 10
 
-# Insert the variable into a new string which we assign to the variable x
+# Insert the variable into a new string which we assign to the variable 'x'
 x = f"There are {types_of_people} types of people"
 
-# define a variable with the string 'binary'
+# Define a variable 'binary' the string binary
 binary = "binary"
 
-# define a variable with the string 'don't
+# Define a variable 'do_not' the string don't
 do_not = "don't"
 
-# Insert those two string variables into a new string and assign it to the variable y
+# Embed those two string variables into a new string and assign it to the variable y
 y = f"Those who know {binary} and those who {do_not}"
 
-# print the value of  x and y variables
+# Print the value of 'x' and 'y' variables
 print(x)
 print(y)
 
-# We print the value for x and y variables, using an f-string
+# We print the value for 'x' and 'y' variables, using an f-string
 print(f"I said: {x}")
 print(f"I also said: '{y}'")
 
@@ -27,12 +27,12 @@ hilarious = False
 # Assign a new string variable with a placeholder
 joke_evaluation = "Isn't that joke so funny so funny?! {}"
 
-# print out the value of the joke_evaluation, using format and passing the hilaroius variable into it
+# Print out the value of the 'joke_evaluation', using format and passing the 'hilaroius' variable into it
 print(joke_evaluation.format(hilarious))
 
-# assign a new variable w and e and assign strings to them
+# Assign a new variable w and e and assign strings to them
 w = "This is the left side of..."
 e = "a string with a right side."
 
-# print out a new string by concatenating the variables w and e
+# Print out a new string by concatenating the variables w and e
 print(w + e)
