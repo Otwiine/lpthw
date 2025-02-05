29c8283e6ace1785f48e9f819cb205df6e8a6789
Otwiine Olweny <otwiine@gmail.com>
2025-02-05 16:20:38 +0300

    Comments and running .close()

--- ex15.py
+++ ex15.py
@@ -1,21 +1,25 @@
+# import argv from sys module
 from sys import argv
 
 # Unpacks argv
 script, filename = argv
 
-# Open the file and assign it to a variable
+# Open the file {filename} and assign it to a variable 'txt'
 txt = open(filename)
 
-# Print out the filename and print out its contents
+# Print out the filename and use .read to print out its contents
 print(f"Here's your file {filename}:")
 print(txt.read())
 
-# Print out instructions and ask for input
+# Print out instructions and ask for input using input()
 print("Type the filename again:")
 file_again = input("> ")
 
-# open the file again and assign it to a new variable
+# Open the file again and assign it to a new variable
 txt_again = open(file_again)
 
 # print out the opened file, again
 print(txt_again.read())
+
+txt.close()
+txt_again.close()
