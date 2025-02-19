7efd19e3a404f80fa748d020989118c450f7bd27
Otwiine Olweny <otwiine@gmail.com>
2025-02-19 09:52:30 +0300

    List indexing

---
+++ ex34.py
@@ -0,0 +1,6 @@
+# Lists are zero indexed, 0 would be the first element
+animals = ['bear', 'tiger', 'penguin', 'zebra']
+people = ['OJO', 'Sulwe', 'Leon', 'Geera']
+
+print(animals[1])
+print(people[2])
