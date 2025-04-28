ffada21090a766f2b350b37f93768861959e7a7b
Otwiine Olweny <otwiine@gmail.com>
2025-04-28 07:52:50 +0300

    Del

--- ex12.py
+++
@@ -1,7 +0,0 @@
-# This simplifies ex11.py by writing code on a singular line, removing end=''
-
-age = input("How old are you? ") # We leave a space  
-height = input("How tall are you? ") # so that the user input
-weight = input("How much do you weigh? ") # is not directly attached to the question
-
-print(f"So, you're {age} years old, {height} tall and {weight} heavy.")
--- ex34.py
+++
@@ -1,6 +0,0 @@
-# Lists are zero indexed, 0 would be the first element
-animals = ['bear', 'tiger', 'penguin', 'zebra']
-people = ['OJO', 'Sulwe', 'Leon', 'Geera']
-
-print(animals[1])
-print(people[2])
