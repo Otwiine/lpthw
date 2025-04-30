9d792339ea88faa431bef80696309c49c53d1bc0
Otwiine Olweny <otwiine@gmail.com>
2025-04-30 08:58:53 +0300

    More Printing

---
+++ ex07.py
@@ -0,0 +1,27 @@
+# Print out the string
+print("Mary had a little lamb.")
+# Print out the string and replace {} with the parameter in .format
+print("Its fleece was white as {}.".format('snow'))
+# Print out the string
+print("And everywhere that Mary went.")
+# Print the . out 10 times
+print("." * 10) # what'd that do?
+
+# Assign single characters to the defined variables
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch end = ' ' at the end. try removing it to see what happens
+# Concateneate the variables and print them out
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')  # end=' ' tells Python to print two spaces after "Cheese" instead of a new line
+print(end7 + end8 + end9 + end10 + end11 + end12)
