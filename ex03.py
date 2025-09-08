c379a727f005ab22465315d58957f64e49ea4ab6
Otwiine Olweny <otwiine@gmail.com>
2025-09-08 10:16:47 +0300

    Numbers and Math

---
+++ ex03.py
@@ -0,0 +1,34 @@
+# Prints a statement
+print("I will now count my chickens:")
+
+# Calculates number of Hens
+print("Hens", 25 + 30 / 6)
+# Calculates number of Roosters
+print("Roosters:", 100 - 25 * 3 % 4)
+
+# Prints a statement
+print("Now I will count the eggs:")
+
+# Calculates the number of eggs
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# Asks a question
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# Performs comparison, Result is boolean
+print(3 + 2 < 5 - 7)
+
+# Asks and calculates
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+# Prints a statement
+print("Oh, that's why it's False.")
+
+# Prints a statement
+print("How about some more.")
+
+# Asks question, compares and gives a Boolean answer
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
