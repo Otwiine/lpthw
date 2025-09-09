fb780392c723c3bb158230b1b58776b1bc8f837f
Otwiine Olweny <otwiine@gmail.com>
2025-09-09 08:17:26 +0300

    More Printing

---
+++ ex07.py
@@ -0,0 +1,28 @@
+# Prints out the string
+print("Mary had a little lamb.")
+# Insert 'snow' into the string using .format
+print("Its fleece was white as {}.".format('snow'))
+# Prints the string
+print("And everywhere that Mary went.")
+# Prints the period character 10x
+print("." * 10) # what'd that do
+
+# Assigns letters to the end Variables (1-12)
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+
+# watch end = ' ' at the end. try removing it to see what happens (No space beetween the words)
+# Concatenates the letters into words
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+print(end7 + end8 + end9 + end10+ end11 + end12)
