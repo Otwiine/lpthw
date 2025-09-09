7409fd4491d63bc3d5286e86bd58785588b4b574
Otwiine Olweny <otwiine@gmail.com>
2025-09-09 12:48:30 +0300

    Parameters, Unpacking, Variables

---
+++ ex13.py
@@ -0,0 +1,13 @@
+from sys import argv
+# read the WYSS section for how to run this
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
+
+# Study Drill
+name = input("What is your name? ")
+location = input("Where do you live? ")
+print(f"Hello {name}! How is {location}?")
