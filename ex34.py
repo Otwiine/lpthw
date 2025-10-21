3f5cf848a7ae4a7d784b6258dea8bf4887e08e47
Otwiine Olweny <otwiine@gmail.com>
2025-10-21 07:58:02 +0300

    Accessing Elements of Lists

---
+++ ex34.py
@@ -0,0 +1,22 @@
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo','whale', 'platypus']
+
+print(animals[1]) # The animal at [1], is the 2nd animal and is 'python3.6'
+print(animals[2]) # The third (3rd) animal, is at [2] and is 'peacock'
+print(animals[0]) # The first (1st) animal, is at [0] and is 'bear'
+print(animals[3]) # The animal at [3], is the 4th animal and is 'kangaroo'
+print(animals[4]) # The fifth (5th) animal, is at [4] and is 'whale'
+print(animals[2]) # The animal at [2], is the 3rd animal and is 'peacock'
+print(animals[5]) # The sixth (6th) animal, is at [5] and is 'platypus'
+print(animals[4]) # The animal at [4], is the 5th animal and is 'whale'
+
+# Study Drill
+languages = ['Python', 'Java', 'C++', 'JavaScript', 'HTML', 'CSS']
+
+print(languages[4]) # The language at [4], is the 5th language and is 'HTML'
+print(languages[1]) # The second (2nd) language, is at [1] and is 'Java'
+print(languages[3]) # The language at [3], is the 4th language and is 'JavaScript'
+print(languages[3]) # The fourth (4th) language, is at [3] and is 'JavaScript'
+print(languages[0]) # The language at [0], is the first language and is 'Python'
+print(languages[5]) # The sixth (6th) language, is at [5] and is 'CSS'
+print(languages[5]) # The language at [5], is the 6th language and is 'CSS'
+print(languages[2]) # The third (3rd) language, is at [2] and is 'C++'
